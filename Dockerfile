262ca2f8252b17ec6339466c44fa960311fee2ad
Henri BERGER <hberger@e1r8p2.42.fr>
2019-11-14 17:39:24 +0100

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,2 @@
-# FROM		: On précise l'image sur la quelle démarer / hériter
-#			  Par conséquent, toutes les commandes shell doivent s'y adapter
-
-# RUN		: Run une commande shell (quelle différence avec CMD ?)
-# 1er RUN 	: permet d'update apt-get et de nettoyer ensuite le gestionnaire de paquets
-
-# WORKDIR	: On définit le dossier de travail
-# 			  Toutes les commandes qui suivront seront exécutées à partir de ce répertoire.
-# CMD		: On exécute le fichier shell
-
-# Pas besoin de Docker Compose car tout doit être dans un seul container
-# Les Dockerfiles sont des fichiers qui permettent de construire une image Docker adaptée à nos besoins
-
-# Classement ressources utiles :
-# La meilleure : https://guillaumebriday.fr/comprendre-et-mettre-en-place-docker
-
-# 1) Les Dockerfiles : https://putaindecode.io/articles/les-dockerfiles/
-# 2) Sur phpMyAdmin : https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-phpmyadmin-on-debian-9
-# 3) Relation DB/Nginx etc ... : https://bigint.fr/blog/2019-05-10/Docker-par-l-exemple
-
 FROM debian:buster
 CMD sh srcs/run.sh
--- srcs/run.sh
+++ srcs/run.sh
@@ -14,26 +14,13 @@ apt-get -y install mariadb-server
 #	- https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-ubuntu-18-04
 #	- https://guillaumebriday.fr/comprendre-et-mettre-en-place-docker
 
-cd
-mkdir -p /var/www/localhost
+cd && mkdir -p /var/www/localhost
 cp nginx.conf /etc/nginx/sites/available/localhost
 ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
 chmod -R 755 /var/www/*
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
-
-# SSL on Nginx
-# Usefull resources :
-# 	-
-
-
-
-
-# ------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------
-
-
 
 # MariaDB
 # Usefull resources :
@@ -45,27 +32,34 @@ echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';" | mysql -u roo
 echo "FLUSH PRIVILEGES;" | mysql -u root
 # Inspired by : https://www.itzgeek.com/how-tos/linux/debian/how-to-install-mariadb-on-debian-10.html#nativepassword
 echo "update mysql.user set plugin = 'mysql_native_password' where user='root';" | mysql -u root
-mysql wordpress -u root --password=  < wordpress.sql
-
-
+cd && mysql wordpress -u root --password=  < wordpress.sql
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
-
-
 
 # Wordpress
 # Usefull resources :
 # 	-
 
-
-cd
-cp wordpress.tar.gz /var/www/localhost/
+cd && cp wordpress.tar.gz /var/www/localhost/
 cd /var/www/localhost/
 tar -xf wordpress.tar.gz
 rm wordpress.tar.gz
 
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
+# SSL on Nginx
+# Usefull resources :
+# 	- https://www.isicca.com/fr/lemp-installer-nginx-php7-mariadb/#c4
+
+mkdir ~/mkcert
+cd ~/mkcert
+wget https://github.com/FiloSottile/mkcert/releases/download/v1.1.2/mkcert-v1.1.2-linux-amd64
+mv mkcert-v1.1.2-linux-amd64 mkcert && \
+chmod +x mkcert
+./mkcert -install
+./mkcert localhost
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
@@ -74,9 +68,9 @@ rm wordpress.tar.gz
 # PHPmyAdmin
 # Usefull resources :
 # 	- https://www.itzgeek.com/how-tos/linux/debian/how-to-install-phpmyadmin-with-nginx-on-debian-10.html
+#	- https://docs.phpmyadmin.net/en/latest/setup.html
 
-cd
-mkdir /var/www/localhost/phpmyadmin
+cd && mkdir /var/www/localhost/phpmyadmin
 wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
 tar -zxvf phpMyAdmin-4.9.0.1-all-languages.tar.gz -C /var/www/localhost/phpmyadmin
 cp /root/config.inc.php /var/www/localhost/phpmyadmin/
